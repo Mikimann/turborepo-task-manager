af12b43453e2d3abb23ea358aec9b231c51af142
Mikiman <jurja.dumitru@gmail.com>
2022-12-20 09:51:28 +0200

    [8] Setup register endpoint

--- apps/server/src/user/dto/create-user.dto.ts
+++ apps/server/src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsNumber, IsString, Matches } from 'class-validator';
+import { MESSAGE, REGEX } from '../../app.utils';
+import * as bcrypt from 'bcrypt';
+
+export class UserDto {
+  // @IsNumber()
+  // id: any;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  @Matches(REGEX.PASSWORD_RULE, {
+    message: MESSAGE.PASSWORD_RULE_MESSAGE,
+  })
+  password: string;
+
+  @IsString()
+  fullName: string;
+
+  // validatePassword(password: string): Promise<boolean> {
+  //   return bcrypt.compare(password, this.password);
+  // }
+}
