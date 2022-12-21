6868d0bd2291418602907abf0f7ba1eec476cd56
Mikiman <jurja.dumitru@gmail.com>
2022-12-21 12:01:27 +0200

    [8] Add interceptor to catch error thrown by db

---
+++ apps/server/src/interceptor/register.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class RegisterInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(
+        catchError(() =>
+          throwError(
+            () =>
+              new HttpException(
+                'Email is already registered',
+                HttpStatus.CONFLICT,
+              ),
+          ),
+        ),
+      );
+  }
+}
