468c9f1c4e9b60c5d463ab816ec2daf8f25d6fff
Mikiman <jurja.dumitru@gmail.com>
2022-12-25 14:36:35 +0200

    [9] Edit auth module

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        // signOptions: { expiresIn: '7200s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
 })
 export class AuthModule {}
